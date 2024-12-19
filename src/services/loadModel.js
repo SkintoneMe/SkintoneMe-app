af46fd61c3655cbc3543b6651f68c0ed0dd03952
SkintoneMe <skintone.me@gmail.com>
2024-12-19 11:55:59 +0700

    Update loadModel.js

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,8 +1,8 @@
-const tf = require("@tensorflow/tfjs-node");
+// const tf = require("@tensorflow/tfjs-node");
 
-const loadModel = async () => {
-  //return tf.loadLayersModel("file://InceptionV3/model.json");
-return tf.loadLayersModel('https://storage.googleapis.com/modelinceptionv3/InceptionV3/model.json');
-}
+// const loadModel = async () => {
+//   //return tf.loadLayersModel("file://InceptionV3/model.json");
+// return tf.loadLayersModel('https://storage.googleapis.com/modelinceptionv3/InceptionV3/model.json');
+// }
 
-module.exports = loadModel;
+// module.exports = loadModel;
